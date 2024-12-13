28a10c3879c5f99a2e454ffc227f4569bdd2c5a4
Deepak Yadav <deepak160895@gmail.com>
2024-12-13 14:24:16 +0530

    Add Update Controller , Add Delete Controller , Add get all blogs controller , add get a blog by id controller

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -18,13 +18,8 @@ const addBlog = asyncHandler(async(req,res)=>{
     }
 
     const blogImageLocalPath = req.file?.path  || ""
-    
-    
-
     const blogImageCloudinary = await uploadCloudinary(blogImageLocalPath)
-    if(!blogImageCloudinary){
-        throw new apiError(400,"Blog Image didnt upload on cloundinary")
-    }
+    
    
     
         const blog = await Blog.create({
@@ -52,8 +47,131 @@ const addBlog = asyncHandler(async(req,res)=>{
 })
 
 //get all blogs
+
+ const blogs = asyncHandler(async(req,res)=>{
+    //get all blofs from db
+   try {
+    const blogs =  await Blog.find()
+ 
+    console.log(blogs)
+    res.status(200).json(
+     new apiResponse(200,blogs,"All blogs fetched successfully")
+    )
+   } catch (error) {
+     throw new apiError(500,error.message)
+    
+   }
+
+
+
+ })
 // get single bog with a id
+const blogById = asyncHandler(async(req,res)=>{
+    // get id from params
+    //search the blog by id in db
+    //send that blog in response
+
+    const {id} = req.params
+    if(!id){
+        throw new apiError(400,"Blog ID is required")
+    }
+
+    try {
+         const blog = await Blog.findById(id)
+         if(!blog){
+            throw new apiError(404,"Blog Not Found")
+         }
+
+         res.status(200).json(
+            new apiResponse(200,
+                blog,
+                `blog by ${id} fetched Successfully`
+            )
+         )
+        
+    } catch (error) {
+        throw new apiError(500,error.message || "failed to fetch the blog")
+        
+    }
+
+})
 // update a blog
+const updateBlog = asyncHandler(async(req,res)=>{
+    const {id} = req.params
+const {title,content,tags,author} = req.body
+const blogImageLocalPath = req.file?.path
+console.log(blogImageLocalPath)
+
+try {
+    const blog = await Blog.findById(id)
+    console.log(blog)
+    if(!blog){
+        throw new apiError(404,"Blog not found")
+    }
+    if(title) blog.title = title
+    if(content) blog.content = content
+    if(tags) blog.tags = tags
+    if(author) blog.author = author
+
+    if(blogImageLocalPath){
+        const blogImageCloudinary = await uploadCloudinary(blogImageLocalPath)
+        if(!blogImageCloudinary){
+            throw new apiError(400,"failed to upload new blog image")
+        }
+
+        blog.blogImage = blogImageCloudinary.url
+    }
+
+    
+
+    
+    const newBlog = await blog.save({validateBeforeSave : false})
+    if(!newBlog){
+        throw new apiError(500,"Something wnet wrong while updating")
+    }
+    
+    res.status(201).json(
+        new apiResponse(200,
+            newBlog,
+            "Update Successfully"
+        )
+    )
+} catch (error) {
+    throw new apiError(400,error.message || "Blog Fetched unsuccessfull")
+    
+}
+
+})
+
+
 // delete a blog
 
-export {addBlog}
+const deleteBlog = asyncHandler(async(req,res)=>{
+    const {id} = req.params
+    if(!id){
+        throw new apiError(400,"Blog id is required to delete the blog")
+    }
+
+    try {
+        await Blog.deleteOne({_id : id})
+        const blog = await Blog.findById(id)
+        if(blog){
+            throw new apiError(500,"Blog delete unsuccessfull")
+        }
+
+        res.status(200).json(
+            new apiResponse(200,
+                "Blog delete Successfull"
+            )
+        )
+
+        
+    } catch (error) {
+        throw new apiError(401,error.message || "blog for delete failed")
+        
+    }
+
+
+})
+
+export {addBlog,blogs,blogById,deleteBlog,updateBlog    }
--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { addBlog } from "../controllers/blog.controller.js";
+import { addBlog,blogs,blogById,deleteBlog,updateBlog } from "../controllers/blog.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import verifyToken from "../middlewares/auth.middlewares.js";
 
 const blogRouter = Router()
 
 blogRouter.route("/addBlog").post(verifyToken,upload.single("blogImage"),addBlog)
+blogRouter.route("/blog").get(blogs)
+blogRouter.route("/blog/:id").get(blogById)
+blogRouter.route("/blog/:id").post(verifyToken,deleteBlog)
+blogRouter.route("/blog/:id").put(verifyToken,upload.single("blogImage"),updateBlog)
 
 
 export {blogRouter}
